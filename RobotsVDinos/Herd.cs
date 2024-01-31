cc1fd8c28bbaa217c59505684a7b3b1e9f724ca4
Lewis Vine <lew.vine@gmail.com>
2024-01-31 12:37:13 -0500

    Filled in Herd constructor

--- RobotsVDinos/Herd.cs
+++ RobotsVDinos/Herd.cs
@@ -8,5 +8,23 @@ namespace RobotsVDinos
 {
     internal class Herd
     {
+        List<Dino> dinos;
+
+        public Herd()
+        {
+            //Create dinos
+            Dino trex = new Dino("trex", 100, 5, 25);
+            Dino stego = new Dino("stego", 100, 15, 10);
+            Dino terra = new Dino("terra", 100, 10, 15);
+
+            //Create dino list
+            this.dinos = new List<Dino>();
+
+            //Add dinos to list
+            dinos.Add(trex);
+            dinos.Add(stego);
+            dinos.Add(terra);
+        }
+
     }
 }
